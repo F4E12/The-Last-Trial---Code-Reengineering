2ea975a44d16985e22a32e0761a4ab9c638a99a3
Jason <therawan.jason@gmail.com>
2025-05-08 16:53:03 +0700

    Refactor animations package

--- src/id/ac/binus/solution/core/animations/CharacterAnimation.java
+++ src/id/ac/binus/solution/core/animations/CharacterAnimation.java
@@ -1,71 +1,47 @@
 package id.ac.binus.solution.core.animations;
 
-
 import java.io.File;
 
 import javafx.scene.image.Image;
 
-public class CharacterAnimation implements IAnimation{
-  private final int DEFAULT_FRAME_WIDTH = 60;
-  private final int DEFAULT_FRAME_HEIGHT = 60;
-
-  public final String spritePath;
-  private final Image spriteImage;
-  private final int spriteColumns;
-  private final long frameDuration;
+public class CharacterAnimation implements IAnimation {
+  private final AnimationConfig animationConfig;
   private final int cropWidth;
   private final int cropHeight;
   private int cyclesCompleted;;
-  
   private int currentFrame;
   private long lastFrameTime;
-  
-  public CharacterAnimation(
-      String spritePath,
-      int spriteColumns,
-      long frameDuration
-  ) {
-    this.spritePath = spritePath;
-    this.spriteImage = new Image(new File(spritePath).toURI().toString());
-    this.spriteColumns = spriteColumns;
-    this.frameDuration = frameDuration;
-    this.cropWidth = DEFAULT_FRAME_WIDTH;
-    this.cropHeight = DEFAULT_FRAME_HEIGHT;
+
+  public CharacterAnimation(AnimationConfig animationConfig) {
+    this.animationConfig = animationConfig;
+    this.cropWidth = 60;
+    this.cropHeight = 60;
     this.cyclesCompleted = 0;
     this.currentFrame = 0;
     this.lastFrameTime = 0;
   }
-  
-  public CharacterAnimation(
-      String spritePath,
-      int spriteColumns,
-      long frameDuration,
-      int cropWidth,
-      int cropHeight
-  ) {
-    this.spritePath = spritePath;
-    this.spriteImage = new Image(new File(spritePath).toURI().toString());
-    this.spriteColumns = spriteColumns;
-    this.frameDuration = frameDuration;
+
+  public CharacterAnimation(AnimationConfig animationConfig, int cropWidth, int cropHeight) {
+    this.animationConfig = animationConfig;
     this.cropWidth = cropWidth;
     this.cropHeight = cropHeight;
-      }
+  }
 
   @Override
   public void start() {
     this.cyclesCompleted = 0;
   }
-  
+
   @Override
   public void update(long currentTime) {
-      if (currentTime - lastFrameTime >= this.frameDuration) {
-          currentFrame++;
-          if (currentFrame >= this.spriteColumns) {
-              currentFrame = 0;
-              ++cyclesCompleted;
-          }
-          lastFrameTime = currentTime;
+    if (currentTime - lastFrameTime >= this.frameDuration) {
+      currentFrame++;
+      if (currentFrame >= this.spriteColumns) {
+        currentFrame = 0;
+        ++cyclesCompleted;
       }
+      lastFrameTime = currentTime;
+    }
   }
 
   @Override
@@ -87,11 +63,10 @@ public class CharacterAnimation implements IAnimation{
   public int getCropHeight() {
     return this.cropHeight;
   }
-  
+
   @Override
   public int getCyclesCompleted() {
     return cyclesCompleted;
   }
 
-  
 }
